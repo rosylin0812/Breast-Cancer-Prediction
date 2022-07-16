cdc2bd0a4716155d30d2521b4a45d143ff938ea4
rosylin0812 <99856012+rosylin0812@users.noreply.github.com>
2022-07-16 19:26:35 -0400

    Update Breast Cancer Prediction.R

--- Breast Cancer Prediction.R
+++ Breast Cancer Prediction.R
@@ -32,9 +32,8 @@ cov(na.omit(breast.df))
 cor(na.omit(breast.df))
 
 ##############  Regression with Principle Components ###################
-### I didn't split the dataset
-
-#### PCA on all 13 variables WITH normalization ####
+### Whole dataset as validation dataset ###
+### PCA on all 31 variables WITH normalization ###
 pcs.cor <- prcomp(na.omit(breast.df), scale. = T) # scale. = T is for normalization
 summary(pcs.cor) 
 
@@ -42,7 +41,7 @@ summary(pcs.cor)
 PoV.cor <- pcs.cor$sdev^2 / sum(pcs.cor$sdev^2)
 barplot(PoV.cor, xlab = "Principal Components", ylab = "Proportion of Variance Explained",ylim=c(0,0.3))
 
-# take first 13 variables from PCA because PC13 the cumulative proportion is 0.97
+# take first 6 variables from PCA because PC6 the cumulative proportion is greater than 0.90
 pcs.data<-data.frame(pcs.cor$x[,1:6], breast.df$diagnosis) 
 
 pcs.data
@@ -59,16 +58,13 @@ confusionMatrix(reg.pred.class, factor(breast.df$diagnosis, levels=c('1', '0')),
 ###### k fold cross validation
 library(boot)
 cv.glm(data= pcs.data, glmfit=reg, K =10)
-#The first component of delta is the average mean-squared error that 
-#you obtain from doing K-fold CV.
+#The first component of delta is the average mean-squared error from doing K-fold CV.
 
-#The second component of delta is the average mean-squared error 
-#that you obtain from doing K-fold CV, but with a bias correction. 
+#The second component of delta is the just the first component of delta with a bias correction. 
 
-#How this is achieved is, initially, the residual sum of squares (RSS) is computed based on 
-#the GLM predicted values and the actual response values for the entire data set.
 
-############################# split the dataset
+
+############################# split the dataset #############################
 
 # Training / Test datasets
 set.seed(1) 
@@ -78,8 +74,21 @@ valid.df <- breast.df[-train.index,]
 
 ##############  Regression without Principle Components ###################
 
+###### stepwise selection
+#bidirection
+library(StepReg)
+formula = diagnosis ~ .
+cancer.glm.step <- stepwiseLogit(formula, data = train.df,  include = NULL, 
+                                 selection = "bidirection",
+                                 select="SL",
+                                 sle=0.0001,
+                                 sls=0.0001,
+                                 sigMethod="Rao")
+cancer.glm.step  # Which variables did it drop?
+
+#Try different formula from the results of stepwise selection
 reg <- glm(diagnosis ~ .,data=train.df, family="binomial")
-reg <- glm(diagnosis ~ radius_se,data=train.df, family="binomial")
+
 reg <- glm(diagnosis ~ perimeter_worst+ concave.points_worst+ texture_mean+ 
              radius_se+ compactness_se,data=train.df, family="binomial")
 
@@ -94,37 +103,24 @@ reg <- glm(diagnosis ~ concave.points_worst+ radius_worst+
              radius_se+ texture_worst,data=train.df, family="binomial")
 
 reg <- glm(diagnosis ~ concave.points_worst+ radius_worst+ 
-             texture_worst,data=train.df, family="binomial")
+             texture_worst,data=train.df, family="binomial")   
 summary(reg) 
 
 ###### k fold cross validation
 library(boot)
 cv.glm(data= train.df, glmfit=reg, K =10)
 
-#########
-library(forecast)
-#Forward
-#install.packages("StepReg") 
-library(StepReg)
-formula = diagnosis ~ .
-cancer.glm.step <- stepwiseLogit(formula, data = train.df,  include = NULL, 
-                                 selection = "bidirection",
-                                 select="SL",
-                                 sle=0.0001,
-                                 sls=0.0001,
-                                 sigMethod="Rao")
-cancer.glm.step  # Which variables did it drop?
-
-#########
+###### regression without PCA perform?
 reg.pred <- predict(reg, valid.df, type='response')
 reg.pred.class <- factor(ifelse(reg.pred > 0.6, 1, 0), levels=c('1', '0'))
 confusionMatrix(reg.pred.class, factor(valid.df$diagnosis, levels=c('1', '0')), positive='1')
 
-##############  K-Neareast     ##################
+##############  K-Neareast     #################
 library(class)
 library(caret)
 # run kNN with k=5
 nn5 <- knn(train.df, valid.df, cl=as.factor(train.df$diagnosis), k=5)
+# kNN performance
 confusionMatrix(as.factor(nn5), as.factor(valid.df$diagnosis))
 
 
